15eeac797bbeb60aac27cac007b9e61b1b9c8db1
joseLopez <kota012011@gmail.com>
2025-05-18 00:20:33 -0500

    estructurando la viasta Comuna

--- src/views/Comunas.vue
+++ src/views/Comunas.vue
@@ -1,45 +1,41 @@
 <template>
-    <div class="container">
-        <h1>Listado de comunas</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Code</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Municipality</th>
-                </tr>
-            </thead>
-        </table>
-        <tbody>
-            <tr v-f="(comuna, index)in comunas" :key="index">
-                <th scope="row">{{ index+1 }}</th>
-                <th >{{comuna.comu_codi }}</th>
-                <th >{{comuna.comu_nomb  }}</th>
-                <th >{{comuna.muni_nomb }}</th>
-            </tr>
-        </tbody>
-
-    </div>
+  <div class="container">
+    <h1>Listado de comunas</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Code</th>
+          <th scope="col">Name</th>
+          <th scope="col">Municipality</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(comuna, index) in comunas" :key="index">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ comuna.comu_codi }}</td>
+          <td>{{ comuna.comu_nomb }}</td>
+          <td>{{ comuna.muni_nomb }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-export default{
-
-    name: 'Comuna',
-    date(){
-        return{
-            comunas:[]
-        }
-    },
-    mounted(){
-        axios
-        .get('http://127.0.0.1:8000/api/comunas')
-        .then(respnse => (this.comunas = response.data.comunas))
-    },
-
+export default {
+  name: 'ComunaV',
+  data() {
+    return {
+      comunas: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://127.0.0.1:8000/api/comunas')
+      .then(response => (this.comunas = response.data.comunas))
+  }
 }
-
 </script>
