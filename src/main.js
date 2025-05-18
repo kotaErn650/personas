70063b69784147fbe838ff967fd8c89df4907623
joseLopez <kota012011@gmail.com>
2025-05-18 16:02:21 -0500

    configurando Main.js con librerias

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import 'bootstrap'
 import'bootstrap/dist/css/bootstrap.min.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons'
+
+
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPhone);
+library.add(faUser);
+library.add(faTrash);
+library.add(faPencil);
+library.add(faTag);
+library.add(faBuilding);
+library.add(faBank);
+library.add(faPlus);
+
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
+
+
+
+
+
+
+
+
+
 
 createApp(App).use(router).mount('#app')
