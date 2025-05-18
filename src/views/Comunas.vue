be677b2204048c255778ddb12f804b4472d369d9
joseLopez <kota012011@gmail.com>
2025-05-18 00:01:20 -0500

    configurando Main con ruta api Comunas

---
+++ src/views/Comunas.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <h1>Listado de comunas</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Code</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Municipality</th>
+                </tr>
+            </thead>
+        </table>
+        <tbody>
+            <tr v-f="(comuna, index)in comunas" :key="index">
+                <th scope="row">{{ index+1 }}</th>
+                <th >{{comuna.comu_codi }}</th>
+                <th >{{comuna.comu_nomb  }}</th>
+                <th >{{comuna.muni_nomb }}</th>
+            </tr>
+        </tbody>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default{
+
+    name: 'Comuna',
+    date(){
+        return{
+            comunas:[]
+        }
+    },
+    mounted(){
+        axios
+        .get('http://127.0.0.1:8000/api/comunas')
+        .then(respnse => (this.comunas = response.data.comunas))
+    },
+
+}
+
+</script>
