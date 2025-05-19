b7a00fbb9356a594ddaccaad59dccd63cbea4518
joseLopez <kota012011@gmail.com>
2025-05-18 19:31:03 -0500

    se corrige el numbre de la carpeta comunas

---
+++ src/components/comunas/NewComuna.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Nueva Comuna</h2>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComuna',
+  data() {
+    return {
+      // Tus variables de datos aquí
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
